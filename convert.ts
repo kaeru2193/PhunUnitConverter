76aebe4a8a36e7a689a84ab5351214747489c52b
kaeru2193 <kaeru2193@gmail.com>
2023-07-27 12:31:55 +0900

    made convert code.

---
+++ convert.ts
@@ -0,0 +1,24 @@
+const phunUnits = {
+    leng: 0.2874,
+    mass: 1.9782,
+    time: 0.3483,
+    temp: 0.6944,
+}
+const SIUnits = {
+    leng: 1,
+    mass: 1,
+    time: 1,
+    temp: 1,
+}
+export function ConvertUnits(value: number, type: string, direction: string) {
+    const phunFactor = phunUnits[type]
+    const SIFactor = SIUnits[type]
+    let convertFactor: number = 0
+    if (direction == "P2E") {
+        convertFactor = phunFactor / SIFactor
+    } else if (direction == "E2P") {
+        convertFactor = SIFactor / phunFactor
+    }
+    const converted = value * convertFactor
+    return converted
+}
